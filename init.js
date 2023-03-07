5c4e765a74fefca50961b8a1b8d69f414db11bdd
Davicho HF <39805786+David1044@users.noreply.github.com>
2023-03-07 09:08:34 -0600

    Add files via upload

---
+++ init.js
@@ -0,0 +1,11 @@
+if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js');
+firebase.initializeApp({
+  "apiKey": "AIzaSyDbJkV105BFU5XlKCxZfSenoHfQo8-sI3A",
+  "appId": "1:50664306111:web:8dc7b3d31769c33371dd0a",
+  "authDomain": "livechat-a16e9.firebaseapp.com",
+  "databaseURL": "",
+  "measurementId": "G-LNW5GCT79N",
+  "messagingSenderId": "50664306111",
+  "projectId": "livechat-a16e9",
+  "storageBucket": "livechat-a16e9.appspot.com"
+});
